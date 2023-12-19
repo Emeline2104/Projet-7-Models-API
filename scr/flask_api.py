201781d0734817f707ac2d69ec08f4fcbc167d0f
Emeline2104 <emeline.tapin@gmail.com>
2023-12-19 18:10:51 +0100

    Supression de la gestion des données dans l'API

--- scr/flask_api.py
+++ scr/flask_api.py
@@ -26,11 +26,9 @@ import joblib
 import lime
 import lime.lime_tabular
 import numpy as np
-import random
 import logging
 from flask import request
 import dill
-import os 
 
 app = Flask(__name__)
 
@@ -41,11 +39,7 @@ logger = logging.getLogger(__name__)
 model_filename = "models/best_model.pkl"
 explainer_filename = "models/explainer_info.dill"
 seuil_filename = "models/meilleur_seuil.txt"
-data_path = "https://projet-7-aws.s3.eu-north-1.amazonaws.com/data_agregg_selec.csv"
 
-
-# Chargement des données
-data = pd.read_csv(data_path)
 
 # Fonctions de chargement
 def load_model(filename):
@@ -107,106 +101,6 @@ def load_explainer(filename):
 model = load_model(model_filename)
 explainer = load_explainer(explainer_filename)
 
-def data_reader(file_path, chunk_size=2000000):
-    # Ouvre le fichier en utilisant l'itérateur de pandas
-    data_reader = pd.read_csv(file_path, chunksize=chunk_size)
-    
-    # Itére à travers les morceaux
-    for chunk in data_reader:
-        # Renvoye chaque morceau
-        yield chunk
-
-def obtenir_informations_brutes_client(client_id, data_reader):
-    # Initialise le dictionnaire pour stocker les informations
-    informations_client = {}
-
-    # Étape 1: Obtenez les informations de l'application (application_train et application_test)
-    informations_application = obtenir_informations_par_table("Data/sampled/application_train_selected.csv", client_id, data_reader)
-    informations_client['informations_application'] = informations_application
-    del informations_application
-
-    # Étape 2: Obtenez les informations du bureau
-    informations_bureau = obtenir_informations_par_table("Data/sampled/bureau_selected.csv", client_id, data_reader)
-    informations_client['informations_bureau'] = informations_bureau
-
-    # Étape 3: Obtenez les informations du bureau_balance en utilisant les informations du bureau
-    bureau_balance_info = obtenir_informations_bureau_balance(informations_bureau, data_reader)
-    informations_client['informations_bureau_balance'] = bureau_balance_info
-    del bureau_balance_info, informations_bureau
-
-    # Étape 4: Obtenez les informations des applications précédentes
-    informations_previous_application = obtenir_informations_par_table("Data/sampled/previous_application_selected.csv", client_id, data_reader)
-    informations_client['informations_previous_application'] = informations_previous_application
-
-    # Étape 5: Obtenez les informations du POS_CASH_balance en utilisant les informations des applications précédentes
-    POS_CASH_balance_info = obtenir_informations_POS_CASH_balance(informations_previous_application, data_reader)
-    informations_client['informations_POS_CASH_balance'] = POS_CASH_balance_info
-    del POS_CASH_balance_info
-
-    # Étape 6: Obtenez les informations des paiements d'installments en utilisant les informations des applications précédentes
-    installments_payments_info = obtenir_informations_installments_payments(informations_previous_application, data_reader)
-    informations_client['informations_installments_payments'] = installments_payments_info
-    del installments_payments_info, informations_previous_application
-
-    # Étape 7: Obtenez les informations du credit_card_balance
-    informations_credit_card_balance = obtenir_informations_par_table("Data/sampled/credit_card_balance_selected.csv", client_id, data_reader)
-    informations_client['informations_credit_card_balance'] = informations_credit_card_balance
-    del informations_credit_card_balance
-
-    return informations_client
-
-
-def obtenir_informations_par_table(url, client_id, data_reader):
-    informations_table = pd.DataFrame()
-    for morceau in data_reader(url):
-        morceau_info = morceau[morceau['SK_ID_CURR'] == client_id]
-        informations_table = pd.concat([informations_table, morceau_info], ignore_index=True)
-    return informations_table.to_dict(orient='records')
-
-def obtenir_informations_bureau_balance(informations_bureau, data_reader):
-    bureau_balance_info = pd.DataFrame()
-    bureau_balance_url = "Data/sampled/bureau_balance_selected.csv"
-    informations_bureau = pd.DataFrame(informations_bureau)
-    informations_bureau = informations_bureau.reset_index(drop=True)
-    for morceau in data_reader(bureau_balance_url):
-        if not informations_bureau.empty:
-            morceau = morceau.reset_index(drop=True)
-            morceau_info = morceau[morceau['SK_ID_BUREAU'].isin(informations_bureau['SK_ID_BUREAU'])]
-        else:
-            morceau_info = pd.DataFrame()
-        bureau_balance_info = pd.concat([bureau_balance_info, morceau_info], ignore_index=True)
-    return bureau_balance_info.to_dict(orient='records')
-
-
-def obtenir_informations_POS_CASH_balance(informations_previous_application, data_reader):
-    POS_CASH_balance_info = pd.DataFrame()
-    POS_CASH_balance_url = "Data/sampled/POS_CASH_balance_selected.csv"
-    informations_previous_application = pd.DataFrame(informations_previous_application)
-    informations_previous_application = informations_previous_application.reset_index(drop=True)
-    for morceau in data_reader(POS_CASH_balance_url):
-        if not informations_previous_application.empty:
-            morceau = morceau.reset_index(drop=True)
-            morceau_info = morceau[morceau['SK_ID_PREV'].isin(informations_previous_application['SK_ID_PREV'])]
-        else:
-            morceau_info = pd.DataFrame()
-        POS_CASH_balance_info = pd.concat([POS_CASH_balance_info, morceau_info], ignore_index=True)
-    return POS_CASH_balance_info.to_dict(orient='records')
-
-
-def obtenir_informations_installments_payments(informations_previous_application, data_reader):
-    installments_payments_info = pd.DataFrame()
-    installments_payments_url = "Data/sampled/installments_payments_selected.csv"
-    informations_previous_application = pd.DataFrame(informations_previous_application)
-    informations_previous_application = informations_previous_application.reset_index(drop=True)
-    for morceau in data_reader(installments_payments_url):
-        if not informations_previous_application.empty:
-            morceau = morceau.reset_index(drop=True)
-            morceau_info = morceau[morceau['SK_ID_PREV'].isin(informations_previous_application['SK_ID_PREV'])]
-        else:
-            morceau_info = pd.DataFrame()
-        installments_payments_info = pd.concat([installments_payments_info, morceau_info], ignore_index=True)
-    return installments_payments_info.to_dict(orient='records')
-
 
 def format_client_data(client_data, donnees_test_path="Data/sampled/test_x_selected_head.csv"):
     """
@@ -219,8 +113,9 @@ def format_client_data(client_data, donnees_test_path="Data/sampled/test_x_selec
     Returns:
     - pd.DataFrame: Les données formatées pour la prédiction.
     """
-    #preprocessor = load_function(preprocessing1_function_filename)
-
+    # Convertissez le dictionnaire en DataFrame
+    client_data = pd.DataFrame.from_dict([client_data])
+    
     # Prétraitement des données du client
     client_data_processed = pre_processing.preprocessor_api(client_data, 'lgbm', balance='None')
 
@@ -249,13 +144,11 @@ def predict_new_client(model, new_client_data, threshold):
         return {'error': 'Client non trouvé'}
     # Prédictions avec le modèle
     # predictions = model.predict(new_client_data)
-    
+    new_client_data = pd.DataFrame(new_client_data)
     # Prédictions de probabilité avec le modèle
     proba_predictions = model.predict_proba(new_client_data)[:, 1]  # Assuming binary classification
-    print(proba_predictions)
     # Filtrer les prédictions en fonction du seuil
     predicted_class = '1' if proba_predictions > threshold else '0'
-    print(predicted_class)
     # Modifier la structure du dictionnaire de sortie
     return int(predicted_class), float(proba_predictions[0])
 
@@ -292,65 +185,21 @@ def calculer_importance_caracteristiques(explainer, new_client_data):
 
     return importance_caracteristiques
 
-def get_client_data(client_id):
-    return data.loc[data['SK_ID_CURR'] == client_id]
-
 # Routes Flask
-@app.route('/get_importance-caracteristiques/<int:client_id>', methods=['GET'])
-def get_importance_caracteristiques(client_id):
-    client_id = int(client_id)  # Convertir client_id en entier
+@app.route('/get_importance-caracteristiques', methods=['POST'])
+def get_importance_caracteristiques():
     try:
-        client_data = get_client_data(client_id)
-        formatted_data = format_client_data(client_data)
+        features_client = request.get_json()
+        formatted_data = format_client_data(features_client)
+        print(formatted_data)
         importance_caracteristiques = calculer_importance_caracteristiques(explainer, formatted_data)
         return jsonify(importance_caracteristiques)
     except Exception as e:
         return jsonify({'error': str(e)})
 
 
-@app.route('/get_target/<client_id>', methods=['GET'])
-def get_target(client_id):
-    """
-    Endpoint pour obtenir la TARGET d'un client spécifique en fonction de son ID.
-
-    Args:
-    - client_id (int): L'ID du client.
-
-    Returns:
-    - JSON: Les données de TARGET du client.
-    """
-    data = pd.read_csv("https://projet-7-aws.s3.eu-north-1.amazonaws.com/data_agregg.csv")
-    client_id = int(client_id)  # Convertir client_id en entier
-
-    if client_id in data['SK_ID_CURR'].values:
-        target = data.loc[data['SK_ID_CURR'] == client_id, 'TARGET'].values[0]
-        response = {'client_id': client_id, 'TARGET': target}
-        return jsonify(response)
-    else:
-        return jsonify({'error': 'Client non trouvé'}), 404
-
-@app.route('/get_client_info/<int:client_id>', methods=['GET'])
-def get_client_info(client_id):
-    """
-    Endpoint pour obtenir des informations supplémentaires sur un client en fonction de son ID.
-
-    Args:
-    - client_id (int): L'ID du client.
-
-    Returns:
-    - JSON: Les informations supplémentaires sur le client.
-    """
-    data = pd.read_csv("https://projet-7-aws.s3.eu-north-1.amazonaws.com/data_agregg.csv")
-
-    client = data[data['SK_ID_CURR'] == client_id].to_dict(orient='records')
-
-    if client:
-        return jsonify(client[0])
-    else:
-        return jsonify({'error': 'Client non trouvé'}), 404
-
-@app.route('/predict/<int:client_id>', methods=['GET'])
-def predict(client_id):
+@app.route('/predict', methods=['POST'])
+def predict():
     """
     Endpoint pour effectuer des prédictions sur les nouvelles données client.
 
@@ -360,9 +209,9 @@ def predict(client_id):
     Returns:
     - JSON: Les prédictions du modèle et les probabilités de prédiction.
     """
-    client_id = int(client_id)  # Convertir client_id en entier
     try:
-        client_data = get_client_data(client_id)
+        # Récupérer les données JSON de la requête
+        client_data = request.get_json()
         formatted_data = format_client_data(client_data)
         with open('models/meilleur_seuil.txt', 'r') as file:
             file_content = file.read()
@@ -376,59 +225,6 @@ def predict(client_id):
         return jsonify({'error': str(e)}), 500
 
     
-@app.route('/informations_client_brut/<int:client_id>', methods=['GET'])
-def obtenir_informations_client(client_id):
-    # Utiliser la fonction de génération pour lire les données par morceaux
-    lecteur_donnees = data_reader
-    client_id = int(client_id)  # Convertir client_id en entier
-    informations_client = obtenir_informations_brutes_client(client_id, lecteur_donnees)
-    return jsonify(informations_client)
-
-def determine_age_group(age):
-    if -(age/365) < 30:
-        return 'Moins de 30 ans'
-    elif -(age/365) <= age < 40:
-        return '30-40 ans'
-    elif -(age/365) <= age < 50:
-        return '40-50 ans'
-    else:
-        return 'Plus de 50 ans'
-    
-@app.route('/get_group_info', methods=['GET'])
-def get_group_info_raw():
-    """
-    Endpoint pour obtenir des informations brutes sur un groupe de clients en fonction de certaines colonnes.
-
-    Returns:
-    - JSON: Les informations brutes sur le groupe de clients.
-    """
-    #data = pd.read_csv("https://projet-7-aws.s3.eu-north-1.amazonaws.com/data_agregg_selec.csv")
-    data = pd.read_csv("Data/sampled/application_train_selected.csv")
-    # Ajoutez ici la logique pour filtrer les données en fonction des colonnes souhaitées (âge, sexe, emploi, etc.)
-    # Par exemple, vous pouvez utiliser les arguments de la requête pour spécifier les filtres
-    #age_filter = request.args.get('age')
-    age_group = request.args.get('age')
-    sex_filter = request.args.get('sex')
-    job_filter = request.args.get('job')
-    # Filtrez les données uniquement sur les colonnes "NAME_CONTRACT_TYPE" et "TARGET"
-    filtered_data = data[['SK_ID_CURR', 'NAME_CONTRACT_TYPE', 'TARGET', 'DAYS_BIRTH', 'CODE_GENDER', 'OCCUPATION_TYPE', 'AMT_CREDIT', 'AMT_ANNUITY', 'AMT_INCOME_TOTAL', ]]
-
-    # Filtrez les données en fonction des filtres spécifiés
-    if age_group:
-        # Filtrer les données par tranche d'âge
-        filtered_data['AGE_GROUP'] = filtered_data['DAYS_BIRTH'].apply(determine_age_group)
-        filtered_data = filtered_data[filtered_data['AGE_GROUP'] == age_group]
-
-    if sex_filter:
-        filtered_data = filtered_data[filtered_data['CODE_GENDER'] == sex_filter]
-    if job_filter:
-        filtered_data = filtered_data[filtered_data['OCCUPATION_TYPE'] == job_filter]
-
-    # Convertissez le DataFrame en JSON
-    filtered_data_json = filtered_data.to_json(orient='records')
-
-    return jsonify(filtered_data_json)
-
 # Route pour obtenir les informations à partir du fichier texte
 @app.route('/get_info_seuil')
 def get_info_from_file():
